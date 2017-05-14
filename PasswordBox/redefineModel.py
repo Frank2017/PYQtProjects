ba0b8bc1d3dea5c7dc9cf3aab44b6de4e6872ab2
Frank2017 <liulei_frank@126.com>
2017-05-14 13:12:52 +0800

    Adding new dialog class for change pswbook password

--- PasswordBox/redefineModel.py
+++ PasswordBox/redefineModel.py
@@ -1,7 +1,8 @@
 # coding=UTF-8
 from PyQt5 import QtGui, QtCore, QtWidgets
+import funclibs
 
-class AddDialog(QtWidgets.QDialog):
+class AddBookDialog(QtWidgets.QDialog):
     def __init__(self, title, data, parent=None):
         QtWidgets.QDialog.__init__(self, parent)
         self.setWindowTitle(title)
@@ -63,13 +64,97 @@ class AddDialog(QtWidgets.QDialog):
             self.lePsw.setText('')
             self.leRePsw.setText('')
         else:
-            super(AddDialog, self).accept()
+            super(AddBookDialog, self).accept()
         pass
 
     def reject(self):
-        super(AddDialog, self).reject()
+        super(AddBookDialog, self).reject()
         pass
 
     def getData(self):
         return [self.lePswName.text(), self.lePsw.text()]
+        pass
+
+
+class EditBookPswDialog(QtWidgets.QDialog):
+    def __init__(self, title, data, bkname, parent=None):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.setWindowTitle(title + '(' + bkname + ')')
+        self.setFixedSize(350, 200)
+        self.pswBookdata = data
+        self.bookName = bkname
+        # 表格布局，用来布局QLabel和QLineEdit及QSpinBox
+        grid = QtWidgets.QGridLayout()
+        # 后面四个数的意义（行，列，行跨度，列跨度）
+        grid.addWidget(QtWidgets.QLabel(u'输入原始密码：', parent=self), 0, 0, 1, 1)
+        self.lePswOld = QtWidgets.QLineEdit(parent=self)
+        self.lePswOld.setEchoMode(QtWidgets.QLineEdit.Password)
+        grid.addWidget(self.lePswOld, 0, 1, 1, 2)
+        grid.addWidget(QtWidgets.QLabel(u'输入新密码：', parent=self), 1, 0, 1, 1)
+        self.lePswNew = QtWidgets.QLineEdit(self)
+        self.lePswNew.setEchoMode(QtWidgets.QLineEdit.Password)
+        grid.addWidget(self.lePswNew, 1, 1, 1, 2)
+        grid.addWidget(QtWidgets.QLabel(u'再次输入新密码：', parent=self), 2, 0, 1, 1)
+        self.leRePswNew = QtWidgets.QLineEdit(self)
+        self.leRePswNew.setEchoMode(QtWidgets.QLineEdit.Password)
+        grid.addWidget(self.leRePswNew, 2, 1, 1, 2)
+        grid.addWidget(QtWidgets.QLabel(u'', parent=self), 3, 0, 1, 3)
+        # 创建ButtonBox，用户确定和取消
+        buttonBox = QtWidgets.QDialogButtonBox(parent=self)
+        buttonBox.setOrientation(QtCore.Qt.Horizontal)  # 设置为水平方向
+        buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)  # 确定和取消两个按钮
+        # 连接信号和槽
+        buttonBox.accepted.connect(self.accept)  # 确定
+        buttonBox.rejected.connect(self.reject)  # 取消
+        # 垂直布局，布局表格及按钮
+        layout = QtWidgets.QVBoxLayout()
+        # 加入前面创建的表格布局
+        layout.addLayout(grid)
+        # 放一个间隔对象美化布局
+        spacerItem = QtWidgets.QSpacerItem(20, 48, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        layout.addItem(spacerItem)
+        # ButtonBox
+        layout.addWidget(buttonBox)
+        self.setLayout(layout)
+        pass
+
+    def accept(self):
+        pswOldVal = self.lePswOld.text()
+        pswNewVal = self.lePswNew.text()
+        rePswNewVal = self.leRePswNew.text()
+        if self.pswBookdata.has_key(self.bookName):
+            # 获得原始密码校验值
+            checkPsw = self.pswBookdata[self.bookName][0]
+        else:
+            checkPsw = ''
+        # 获得输入原始密码的校验值
+        pswOldValMd5 = funclibs.getMD5(pswOldVal)
+        if pswOldVal == '':
+            QtWidgets.QMessageBox.information(self, u'错误', u'请输入原始密码')
+            self.lePswOld.setText('')
+            self.lePswOld.setFocus()
+        elif pswNewVal == '':
+            QtWidgets.QMessageBox.information(self, u'错误', u'请输入新密码')
+            self.lePswNew.setText('')
+            self.leRePswNew.setText('')
+            self.lePswNew.setFocus()
+        elif checkPsw != pswOldValMd5:
+            QtWidgets.QMessageBox.information(self, u'错误', u'请输入正确的原始密码')
+            self.lePswOld.setText('')
+            self.lePswOld.setFocus()
+        elif pswNewVal != rePswNewVal:
+            QtWidgets.QMessageBox.information(self, u'错误', u'输入两次新密码不一致')
+            self.lePswNew.setText('')
+            self.leRePswNew.setText('')
+            self.lePswNew.setFocus()
+        else:
+            super(EditBookPswDialog, self).accept()
+        pass
+
+    def reject(self):
+        super(EditBookPswDialog, self).reject()
+        pass
+
+    def getData(self):
+        return self.lePswNew.text()
         pass
